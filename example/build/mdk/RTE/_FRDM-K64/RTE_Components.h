79207468627b7a3a8c409e94019457b6d2729884
GorgonMeducer <embedded_zhuoran@Hotmail.com>
2018-02-08 17:12:43 +0000

    Update Example Code
    - Add support for FRDM-K64
        - TODO: USART driver pointing

--- example/build/mdk/RTE/_FRDM-K64/RTE_Components.h
+++ example/build/mdk/RTE/_FRDM-K64/RTE_Components.h
@@ -4,7 +4,7 @@
  *      *** Do not modify ! ***
  *
  * Project: 'Blinky' 
- * Target:  'V2M-MPS2' 
+ * Target:  'FRDM-K64' 
  */
 
 #ifndef RTE_COMPONENTS_H
@@ -14,10 +14,11 @@
 /*
  * Define the Device Header File: 
  */
-#define CMSIS_device_header "CMSDK_CM7_SP.h"
+#define CMSIS_device_header "MK64F12.h"
 
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
-#define RTE_Drivers_USART
 
 #endif /* RTE_COMPONENTS_H */
