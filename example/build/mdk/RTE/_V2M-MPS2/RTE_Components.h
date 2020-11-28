188e9c14d432c07dfe00ab48166f350a5bc7a9d2
Gabriel Wang <embedded_zhuoran@hotmail.com>
2020-11-28 10:40:40 +0000

    apply PLOOC v4.5.7

--- example/build/mdk/RTE/_V2M-MPS2/RTE_Components.h
+++ example/build/mdk/RTE/_V2M-MPS2/RTE_Components.h
@@ -1,27 +1,27 @@
-
-/*
- * Auto generated Run-Time-Environment Configuration File
- *      *** Do not modify ! ***
- *
- * Project: 'Blinky' 
- * Target:  'V2M-MPS2' 
- */
-
-#ifndef RTE_COMPONENTS_H
-#define RTE_COMPONENTS_H
-
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "CMSDK_CM7_SP.h"
-
-/*  Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
-#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
-          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
-/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
-#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
-
-
-#endif /* RTE_COMPONENTS_H */
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'V2M-MPS2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "CMSDK_CM7_SP.h"
+
+/* Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+
+
+#endif /* RTE_COMPONENTS_H */
