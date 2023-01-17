8736d8d22b135850ae12ca24472e565a2f74b77f
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2023-01-17 20:50:23 +0800

    Update connect.h

--- sources/gmsi/utilities/preprocessor/connect.h
+++ sources/gmsi/utilities/preprocessor/connect.h
@@ -32,21 +32,22 @@
                                                     __A##__B##__C##__D##__E##__F##__G##__H
 #define __CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I)                 \
                                                     __A##__B##__C##__D##__E##__F##__G##__H##__I
-                                                    
+
+#define ALT_CONNECT2(__A, __B)              __CONNECT2(__A, __B)
 #define CONNECT2(__A, __B)                  __CONNECT2(__A, __B)
 #define CONNECT3(__A, __B, __C)             __CONNECT3(__A, __B, __C)
 #define CONNECT4(__A, __B, __C, __D)        __CONNECT4(__A, __B, __C, __D)
-#define CONNECT5(__A, __B, __C, __D, __E)   __CONNECT5(__A, __B, __C, __D, __E)       
+#define CONNECT5(__A, __B, __C, __D, __E)   __CONNECT5(__A, __B, __C, __D, __E)
 #define CONNECT6(__A, __B, __C, __D, __E, __F)                                  \
-                                            __CONNECT6(__A, __B, __C, __D, __E, __F) 
+                                            __CONNECT6(__A, __B, __C, __D, __E, __F)
 #define CONNECT7(__A, __B, __C, __D, __E, __F, __G)                             \
-                                            __CONNECT7(__A, __B, __C, __D, __E, __F, __G) 
+                                            __CONNECT7(__A, __B, __C, __D, __E, __F, __G)
 #define CONNECT8(__A, __B, __C, __D, __E, __F, __G, __H)                        \
-                                            __CONNECT8(__A, __B, __C, __D, __E, __F, __G, __H) 
+                                            __CONNECT8(__A, __B, __C, __D, __E, __F, __G, __H)
 #define CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I)                   \
                                             __CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I)
-       
-#define CONNECT(...)                                                            \
-            CONNECT2(CONNECT, __PLOOC_VA_NUM_ARGS(__VA_ARGS__))(__VA_ARGS__)
 
-#endif  // _TPASTE_H_
+#define CONNECT(...)                                                            \
+            ALT_CONNECT2(CONNECT, __PLOOC_VA_NUM_ARGS(__VA_ARGS__))(__VA_ARGS__)
+
+#endif
